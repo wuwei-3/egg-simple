f9195dc6564313aa52d327d16cc3ab9cb3dc452a
wuwei <weiwu@yitong.com.cn>
2022-03-02 20:45:22 +0800

    user edit

--- app/router.js
+++ app/router.js
@@ -8,8 +8,12 @@ module.exports = app => {
   /* 登录鉴权 */
   router.post('/login', controller.login.login);
   /* 目前只对api进行全局错误拦截 */
+  // 用户增删改查
   router.get('/api/userList', controller.user.getUserList);
   router.get('/api/addUser', controller.user.addUser);
   router.post('/api/updateUser', controller.user.editUser);
   router.get('/api/deleteUser', controller.user.delUser);
+  //  商品增删改查
+  // 消费增删改查
+  // 
 };
